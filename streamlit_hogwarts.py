f8885484da1a03ff091631ca7e40611e7bd6c807
bparedes21 <gestionorganizacion17@gmail.com>
2023-09-18 18:26:24 -0300

    ´prueba7

--- streamlit_hogwarts.py
+++ streamlit_hogwarts.py
@@ -5,9 +5,8 @@ import datetime
 url = 'https://api-hogwarts.vercel.app/coin?coin='
 
 today = datetime.datetime.now()
-d = st.date_input("When's your birthday", datetime.date(today))
+st.write('Your birthday is:', today)
 
-st.write('Your birthday is:', d)
 url +=str(d)
 x = requests.post(url)
 
