58521c443d8f090913935b6926cad83bdc2d9350
bparedes21 <gestionorganizacion17@gmail.com>
2023-09-19 19:25:26 -0300

    subida9

--- streamlit_hogwarts.py
+++ streamlit_hogwarts.py
@@ -1,18 +1,19 @@
 import requests
 import streamlit as st
 import datetime
-
+import json
 url = 'https://api-hogwarts.vercel.app/CasaHogward?fecha='
 
 
 start_date = datetime.date(year=1950,month=1,day=1)
 end_date = datetime.datetime.now().date()
 d = st.date_input("When's your birthday", min_value=start_date,max_value= end_date)
-var='Your birthday is:'+str(d)
-st.write(var, d)
-
+var='La feccha elegida es: '+str(d)
+st.write(var)
+##enlace + fecha de para el post
 url +=str(d)
-x = requests.post(url)
+response= requests.post(url)
 
-
-st.title(x.text)
+response_dict = json.loads(response.text)
+num_casa=response_dict["CASA"]
+st.title(num_casa)
