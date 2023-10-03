38d1083ae93fc74cea1094cd2992db6ae622c885
bparedes21 <gestionorganizacion17@gmail.com>
2023-10-03 20:24:45 -0300

    pi31

--- streamlit_hogwarts.py
+++ streamlit_hogwarts.py
@@ -33,46 +33,46 @@ except ValueError as error:
     image_new=image.resize((400,400))
     st.image(image_new, caption='HP')
 
-
-
-if boton_escudo:
-   
-
-    if(num_casa==1)or(num_casa==5)or(num_casa==9):
+if(num_casa==1)or(num_casa==5)or(num_casa==9):
         image = Image.open('gry.png')
-        image_new=image.resize((350,400))
-        st.image(image_new, caption='Gryffindor')
+       
+        scaption_var='Gryffindor'
         Casa_ho="Gryffindor"
 
         significado_casa="Valentía, coraje y determinación"
         
-    elif(num_casa==2)or(num_casa==6):
-        image = Image.open("hu.png")
-        image_new=image.resize((350,400))
-        st.image(image_new, caption='Hufflepuff')
+elif(num_casa==2)or(num_casa==6):
+    image = Image.open("hu.png")
+    caption_var='Hufflepuff'
 
-        Casa_ho="Hufflepuff"
-        significado_casa="Lealtad, paciencia y trabajo duro."
-    elif(num_casa==3)or(num_casa==7):
-        image = Image.open("ra.png")
-        image_new=image.resize((350,400))
-        st.image(image_new, caption='Ravenclaw')
+    Casa_ho="Hufflepuff"
+    significado_casa="Lealtad, paciencia y trabajo duro."
 
-        Casa_ho="Ravenclaw"
-        significado_casa="Inteligencia, sabiduría y creatividad. Asociados a los números"
-    elif(num_casa==4)or(num_casa==8):
-        image = Image.open("sly.png")
-        image_new=image.resize((350,400))
-        st.image(image_new, caption='Slytherin')
-        Casa_ho="Slytherin"
-        significado_casa="Astucia, ambición y determinación."
-        
-    if boton_significado:
-        boton_escudo=False
-        
-        num_casa=str(num_casa)
-        st.header("El resultado de la suma de la fecha de nacimiento es: "+num_casa)
+elif(num_casa==3)or(num_casa==7):
+    image = Image.open("ra.png")
+    
+    caption_var='Ravenclaw'
 
-        st.header("La casa correspondiente es: "+Casa_ho)
-        #https://gist.github.com/rxaviers/7360908
-        st.header("La casa representa: "+significado_casa)
+    Casa_ho="Ravenclaw"
+    significado_casa="Inteligencia, sabiduría y creatividad. Asociados a los números"
+elif(num_casa==4)or(num_casa==8):
+    image = Image.open("sly.png")
+    caption_var='Slytherin'
+    
+    
+    Casa_ho="Slytherin"
+    significado_casa="Astucia, ambición y determinación."
+
+if boton_escudo:
+    image_new=image.resize((350,400))
+    st.image(image_new, caption=caption_var)
+
+if boton_significado:
+    boton_escudo=False
+    
+    num_casa=str(num_casa)
+    st.header("El resultado de la suma de la fecha de nacimiento es: "+num_casa)
+
+    st.header("La casa correspondiente es: "+Casa_ho)
+    #https://gist.github.com/rxaviers/7360908
+    st.header("La casa representa: "+significado_casa)
